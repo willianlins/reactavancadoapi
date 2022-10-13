2a42bfa249d9c0b5359785d31073c4de383d0560
Willian lins araujo <williannlins@gmail.com>
2022-10-13 16:35:14 -0300

    erro na integracao postgree strapi

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi-api'),
+      database: env('DATABASE_NAME', 'strapiapi'),
       user: env('DATABASE_USERNAME', 'strapi-api'),
       password: env('DATABASE_PASSWORD', 'strapi123'),
       ssl: env.bool('DATABASE_SSL', false),
